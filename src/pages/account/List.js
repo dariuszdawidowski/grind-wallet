0e76a15dee6cacb0b4371bdcc794596d07712cd8
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-10-01 22:31:00 +0200

    Card sheet. General styling.

--- src/pages/account/List.js
+++ src/pages/account/List.js
@@ -3,26 +3,40 @@ import { Button } from '../../widgets/Button.js';
 import { Card } from '../../widgets/Card.js';
 import { SheetNewAccount } from './NewAccount.js';
 import { SheetImportAccount } from './ImportAccount.js';
+const { version } = require('../../../package.json');
 
 
 export class PageListAccounts extends Component {
 
-    constructor(app, args) {
+    constructor(app) {
         super(app);
 
         // Build
         this.element.classList.add('page');
         this.element.innerHTML = `
-            <h1 style="text-align: center;">Grind Wallet</h1>
+            <h1 style="text-align: center; margin-bottom: 25px;">Grind Wallet <span>v${version}</span></h1>
         `;
 
         // Cards
         this.append(new Card({
             app,
-            id: 'account-card-XXXXX',
+            id: 'account-aa39b30e61dd2b181a5f2df050d3f0de1ca8811ac7a352a3af97b0ffb29f423a',
+            name: 'ICP #1',
+            balance: 0.0,
+            account: 'aa39b30e61dd2b181a5f2df050d3f0de1ca8811ac7a352a3af97b0ffb29f423a',
+            logo: 'assets/IC_logo_horizontal.svg',
             click: () => {
             }
         }));
+
+        // Separator
+        this.element.append(document.createElement('hr'));
+
+        // Info
+        const info = document.createElement('div');
+        info.style.margin = '17px auto 7px auto';
+        info.innerHTML = 'Create next one or import an existing one';
+        this.element.append(info);
 
         // Buttons
         this.append(new Button({
--- src/widgets/Card.js
+++ src/widgets/Card.js
@@ -1,4 +1,5 @@
 import { Component } from '../Boost.js';
+import { Button } from './Button.js';
 
 
 export class Card extends Component {
@@ -10,13 +11,13 @@ export class Card extends Component {
         this.element.id = args.id;
         this.element.classList.add('card');
         this.element.innerHTML = `
-            <div class="name">ICP #1</div>
+            <div class="name">${args.name}</div>
             <div class="subname">CRYPTOCURRENCY WALLET</div>
             <div class="currency">ICP</div>
-            <div class="amount">0.000000</div>
-            <div class="account1">aa39 b30e 61dd 2b18 1a5f 2df0</div>
-            <div class="account2">50d3 f0de 1ca8 811a c7a3 52a3 af97 b0ff b29f 423a</div>
-            <img class="logo" src="assets/IC_logo_horizontal.svg">
+            <div class="amount">${args.balance !== null ? this.formatCurrency(args.balance, 8) : 'Fetching...'}</div>
+            <div class="account1">${this.formatWithSpaces(args.account.substring(0, 24), 4)}</div>
+            <div class="account2">${this.formatWithSpaces(args.account.substring(24), 4)}</div>
+            <img class="logo" src="${args.logo}">
             <div class="open">
                 Click to open
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -26,7 +27,78 @@ export class Card extends Component {
             </div>
         `;
 
+        // Bottom Sheet
+        this.sheet = new SheetCardAccount(args.app);
+
+        // Events
+        this.event.on({
+            id: `card:${this.element.id}`,
+            type: 'click',
+            callback: () => {
+                this.app.card.show({
+                    title: args.name,
+                    content: this.sheet.element
+                });
+            }
+        });
+
+    }
+
+    formatCurrency(value, fixed = 2) {
+        const s = value.toFixed(fixed).toString()
+        const [whole, fractional] = s.split('.');
+        return this.formatWithSpaces(whole, 3, false) + '.' + fractional;
+    }
+
+    formatWithSpaces(inputString, everyNCharacters, fromLeft = true) {
+        let result = '';
+
+        if (fromLeft) {
+            for (let i = 0; i < inputString.length; i++) {
+                result += inputString[i];
+
+                if ((i + 1) % everyNCharacters === 0 && i !== inputString.length - 1) {
+                    result += ' ';
+                }
+            }
+        } else {
+            for (let i = inputString.length - 1, j = 1; i >= 0; i--, j++) {
+                result = inputString[i] + result;
+
+                if (j % everyNCharacters === 0 && i !== 0) {
+                    result = ' ' + result;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
+
+class SheetCardAccount extends Component {
+
+    constructor(app, args) {
+        super(app);
+
+        // Build
+        this.element.classList.add('form');
+        this.element.innerHTML = `
+            <h3>
+               An account
+            </h3>
+        `;
+
+        // Buttons
+        this.append(new Button({
+            app,
+            id: 'create-account-proceed',
+            text: 'Proceed',
+            click: () => {
+            }
+        }));
 
     }
 
 }
+

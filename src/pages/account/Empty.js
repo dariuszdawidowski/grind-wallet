062e98b86a8f5a09784c91fd2be4da9e315e27bd
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-10-03 17:07:24 +0200

    Wallet icon

--- src/pages/account/Empty.js
+++ src/pages/account/Empty.js
@@ -14,11 +14,7 @@ export class PageEmpty extends Component {
         this.element.innerHTML = `
             <h1 style="text-align: center;">Welcome to Grind Wallet<br><img src="assets/icon728.png" width="155"></h1>
             <h2>
-                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none" stroke="white" stroke-width="3">
-                    <rect x="5" y="15" width="40" height="25" rx="3" ry="3" fill="none" stroke="white" stroke-width="3"/>
-                    <rect x="5" y="10" width="35" height="10" fill="none" stroke="white" stroke-width="3"/>
-                    <circle cx="38" cy="25" r="2" fill="white" />
-                  </svg>
+                <img src="assets/material-design-icons/wallet.svg">
                 <br>
                 You don't have any wallet,<br>
                 create a new one or import an existing one<br>

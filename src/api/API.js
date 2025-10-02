231532b7425ed568e145148f0ae0b7b6ede89ba3
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2025-10-02 19:55:22 +0200

    Dir

---
+++ src/api/API.js
@@ -0,0 +1,22 @@
+/**
+ * API for developers
+ */
+
+export class API {
+
+    constructor() {
+
+    }
+
+    /**
+     * Request connection to Grind Wallet plugin
+     * @param whitelist: ['Canister ID', ...] - Will ask user for permission to connect to these canisters
+     * @param host: string URL - Optional ICP host for local development (default is https://icp0.io)
+     * @param timeout: Number in ms - connection timeout (default 5000)
+     */
+
+    async requestConnect(args) {
+
+    }
+
+}

716d80a2dc220ff6a918d0787fa9c3c9332cdf8e
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2025-10-28 10:18:49 +0100

    Dev mode.

--- src/chrome-extension/popup/dev-mode.js
+++ src/chrome-extension/popup/dev-mode.js
@@ -1,6 +1,6 @@
-// const devBanner = document.createElement('div');
-// devBanner.id = 'top-banner';
-// devBanner.textContent = `DEV MODE`;
-// devBanner.style.backgroundColor = '#ff9e36ff';
-// document.body.insertBefore(devBanner, document.body.firstChild);
+const devBanner = document.createElement('div');
+devBanner.id = 'top-banner';
+devBanner.textContent = `DEV MODE`;
+devBanner.style.backgroundColor = '#ff9e36ff';
+document.body.insertBefore(devBanner, document.body.firstChild);
 console.info(`🛠️ DEV MODE`);

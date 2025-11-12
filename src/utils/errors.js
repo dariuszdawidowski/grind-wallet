089893181071041d4664bd77b560464f7931f8ff
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2025-11-12 01:45:22 +0100

    Catch console error.

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -5,12 +5,21 @@
 export class ErrorSystem {
 
     constructor() {
+        // Global error handler
         window.addEventListener('error', (ev) => {
             console.log('🪲', ev?.error?.stack);
         });
 
+        // Global promise rejection handler
         window.addEventListener('unhandledrejection', (ev) => {
             console.log('🐞', ev?.reason?.stack);
         });
+
+        // Console error override
+        const origConsoleError = console.error;
+        console.error = function(...args){
+            origConsoleError.apply(console, args);
+        }
+        
     }
 }

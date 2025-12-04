d1369da80bc62641bcbd6f189d3d1f24bdc21639
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2025-12-04 18:42:18 +0100

    Send errors to backend.

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,6 +9,9 @@ export class Config {
     constructor({ app }) {
         this.app = app;
 
+        // Generete anonymous client ID for this installation
+        this.clientId = null;
+
         // Properties
         this.hideBalances = false;
         this.showScrolls = true;
@@ -21,7 +24,12 @@ export class Config {
      */
 
     async load() {
-        const storage = await browser.storage.local.get(['cfg:hide', 'cfg:scroll', 'cfg:session', 'cfg:errors']);
+        const storage = await browser.storage.local.get(['cfg:cid', 'cfg:hide', 'cfg:scroll', 'cfg:session', 'cfg:errors']);
+        if (storage.hasOwnProperty('cfg:cid')) this.clientId = storage['cfg:cid'];
+        else {
+            this.clientId = crypto.randomUUID();
+            await browser.storage.local.set({ 'cfg:cid': this.clientId });
+        }
         if (storage.hasOwnProperty('cfg:hide')) this.hideBalances = storage['cfg:hide'];
         if (storage.hasOwnProperty('cfg:scroll')) this.showScrolls = storage['cfg:scroll'];
         if (storage.hasOwnProperty('cfg:session')) this.sessionTimeout = storage['cfg:session'];
--- src/utils/errors.js
+++ src/utils/errors.js
@@ -7,38 +7,45 @@ import { LogBase } from './logbase.js';
 export class ErrorSystem extends LogBase {
 
     constructor(args) {
-        super(args);
+        super();
 
-        const timestamp = new Date().getTime();
+        this.app = args.app;
 
         // Global error handler
         window.addEventListener('error', (ev) => {
-            this.add(args.app.version, timestamp, ev?.error?.stack || ev.message || 'Unknown error');
+            this.error(ev?.error?.stack || ev.message || 'Unknown error');
         });
 
         // Global promise rejection handler
         window.addEventListener('unhandledrejection', (ev) => {
-            this.add(args.app.version, timestamp, ev?.reason?.stack || 'Unknown error');
+            this.error(ev?.reason?.stack || 'Unknown error');
         });
 
         // Console error override
         const origConsoleError = console.error;
-        const self = this;
-        const appVersion = args.app.version;
-        console.error = function(...errorArgs) {
-            self.add(appVersion, timestamp, errorArgs.map(arg => (typeof arg === 'string') ? arg : JSON.stringify(arg)).join(' '));
+        console.error = (...errorArgs) => {
+            this.error(errorArgs.map(arg => (typeof arg === 'string') ? arg : JSON.stringify(arg)).join(' '));
             origConsoleError.apply(console, errorArgs);
         }
         
     }
 
     /**
+     * Add error
+     */
+
+    error(msg) {
+        const timestamp = new Date().getTime();
+        this.add(this.app.version, timestamp, msg);
+        if (this.app.config.sendErrors) this.app.backend.writeErrorLog(this.app.config.clientId, msg);
+    }
+
+    /**
      * Get filtered or all logs
      */
 
-    async get(storeName, args = null) {
-        const logs = await this.load(storeName);
-        return logs;
+    async get() {
+        return await this.load(this.app.version);
     }
 
 }
